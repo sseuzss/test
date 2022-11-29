ed1d3555f781fb9465d3149f80d657be0a602548
sseuzss <serge2091@gmail.com>
2022-11-29 15:22:33 +0500

    Test build all without docker

--- build/package/server/build-local.sh
+++ build/package/server/build-local.sh
@@ -2,4 +2,5 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "${DIR}/build-$(go env GOOS)-$(go env GOARCH).sh"
 "${DIR}"/build-$(go env GOOS)-$(go env GOARCH).sh
